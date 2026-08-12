add8af7f0e06118b770b5d389a6a561c5f021f22
Blazepaws <lsuchy@proton.me>
2026-08-12 22:05:35 +0000

    Finalized the auth with the correct bnet id

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,14 +1,9 @@
-import NextAuth, { type Session, type NextAuthConfig } from "next-auth"
+import NextAuth, { type Session, type NextAuthConfig, Account, User, Profile } from "next-auth"
 import type { JWT } from "next-auth/jwt"
 import BattleNetProvider, { type BattleNetIssuer } from "next-auth/providers/battlenet"
 import CredentialsProvider from "next-auth/providers/credentials"
 import prisma from "./prisma"
 
-interface SessionUser {
-  id?: string
-  battleNetTag?: string
-  battleNetId?: string
-}
 
 export const authOptions: NextAuthConfig = {
   providers: [
@@ -21,65 +16,67 @@ export const authOptions: NextAuthConfig = {
   ],
   callbacks: {
 
-    async signIn({ user, account }) {
-      if (account?.provider === "battlenet") {
+    async signIn({ user, account, profile }: { user: User & { dbId?: number }; account?: Account | null; profile?: Profile }) {
+      console.log("signIn callback called with user:", user, "account:", account, "profile:", profile)
 
-        if (!user.id) {
-          return false
+      if (account?.provider === "battlenet" && user?.id) {
+        // When a user signs in with Battle.net, we need to make sure
+        // they have an account in our database. If they don't, we create one.
+
+        const battleNetId = account?.providerAccountId;
+        const battleNetTag = (profile as any)?.battletag;
+
+        if (!battleNetId) {
+          console.error("Battle.net account ID is missing.")
+          return false;
+        }
+        if (!battleNetTag) {
+          console.error("Battle.net tag is missing.")
+          return false;
         }
 
-        const battleNetId = user.id.toString()
-        const battleNetTag = user.name ?? ""
-        const issuer = process.env.BATTLENET_ISSUER ?? ""
-        const region = issuer.includes("eu.battle.net") ? "EU" : issuer.includes("kr.battle.net") ? "KR" : issuer.includes("tw.battle.net") ? "TW" : "US"
+        const region = "EU" // TODO: If we need to access other Blizzard APIs we need to deal with regionality properly.
 
-        await prisma.user.upsert({
-          where: { battleNetId },
-          create: { battleNetId, battleNetTag, region },
-          update: { battleNetTag },
-        })
+        try {
+          const dbUser = await prisma.user.upsert({
+            where: { battleNetId },
+            create: { battleNetId, battleNetTag, region },
+            update: { battleNetTag },
+          });
+          // Pass our own user ID from the database down.
+          user.dbId = dbUser.id;
+        } catch (error) {
+          console.error("Error upserting user in database:", error)
+          return false;
+        }
       }
 
-      return true
+      return true;
     },
 
-    async jwt({ token, user }: { token: JWT & { battleNetTag?: string; battleNetId?: string }; user?: any }) {
+    async jwt({ token, account, user }: { token: JWT & { id?: number, bnetAccessToken?: string }; account?: Account | null; user: User & { dbId?: number } }) {
 
-      console.log("=== JWT CALLBACK ===")
+      console.log("JWT callback called with token:", token, "user:", user)
       console.log("token BEFORE:", token)
-      console.log("user:", user)
 
-      if (user?.id) {
-        const dbUser = await prisma.user.findUnique({
-          where: {
-            battleNetId: user.id.toString(),
-          },
-        })
+      // Again, pass our own ID down to the token, so we can eventually put it in the session.
+      token.id = user?.dbId;
 
-        if (dbUser) {
-          token.sub = dbUser.id.toString()
-          token.battleNetId = dbUser.battleNetId
-          token.battleNetTag = dbUser.battleNetTag
-        }
+      // Also put the bnet API access token in here.
+      if (account?.provider === "battlenet") {
+        token.bnetAccessToken = account?.access_token;
       }
-
+      
       console.log("token AFTER:", token)
       return token
     },
 
-    async session({ session, token }: { session: Session; token: JWT & { battleNetTag?: string; battleNetId?: string } }) {
+    async session({ session, token }: { session: Session; token: JWT & { id?: number } }) {
 
+      // Create a session that has our own user ID and the display name.
       if (session.user) {
-        const sessionUser = session.user as SessionUser
-
-        if (token?.sub) {
-          sessionUser.id = token.sub;
-        }
-
-        if (session.user) {
-          sessionUser.battleNetTag = token.battleNetTag;
-          sessionUser.battleNetId = token.battleNetId;
-        }
+        session.user.id = token.id?.toString();
+        session.user.name = token.name;
       }
 
       return session
@@ -87,6 +84,8 @@ export const authOptions: NextAuthConfig = {
   },
 }
 
+// In development we add an escape hatch to allow logging in without authentication.
+// DANGER: DO NOT ENABLE THIS IN PRODUCTION. It allows anyone to log in as the test user.
 if (process.env.NODE_ENV === "development") {
   authOptions.providers.push(
     CredentialsProvider({
